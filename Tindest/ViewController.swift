544846dfd6ebc8be3d1d237715edce0d9daa60c0
takusemba <taku1008ele@gmail.com>
2016-12-07 14:44:02 +0900

    Initial Commit

---
+++ Tindest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tindest
+//
+//  Created by TakuSemba on 2016/12/07.
+//  Copyright © 2016年 TakuSemba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
