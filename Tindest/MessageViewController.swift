3c76923a1b42c0056a25a16c6b545d6776e7aef3
takusemba <taku1008ele@gmail.com>
2016-12-13 18:44:05 +0900

    fix layout

--- Tindest/MessageViewController.swift
+++ Tindest/MessageViewController.swift
@@ -20,6 +20,9 @@ class MessageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        
+        tableView.backgroundColor = UIColor.white
         tableView.dataSource = self
         tableView.delegate = self
         
